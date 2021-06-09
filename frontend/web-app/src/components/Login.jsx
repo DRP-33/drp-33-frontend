b84edbffec0af55a982981b3ef0b5c690885c1df
unknown <pmk19@ic.ac.uk>
2021-06-09 14:44:25 +0200

    Login component finished todo: post request to backend

--- frontend/web-app/src/components/Login.jsx
+++ frontend/web-app/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useHistory } from "react-router-dom";
 
 const LoginViewStyle = {
     height: "100vh",
@@ -16,12 +17,25 @@ const FormStyle = {
 }
 
 function Login() {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const history = useHistory();
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        history.push("/map");
+    }
+
     return (
         <div style={LoginViewStyle}>
-            <Form style={FormStyle}>
+            <Form style={FormStyle} onSubmit={handleSubmit}>
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                     <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                     </Form.Text>
@@ -29,12 +43,9 @@ function Login() {
 
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" disabled={!validateForm()}>
                     Submit
                 </Button>
             </Form>
